8f21e75a7faecfed6d801caa9dcc0d2c1a07438c
Salma <salma.charad@outlook.com>
2020-11-16 16:26:39 +0100

    Paths and Cycles
    Added/Updated code comments.

--- Cycles and paths.py
+++ Cycles and paths.py
@@ -1,5 +1,4 @@
 import sys
-# Oki let's make a minor change to try with
 # ask user for number of vertices
 verts = eval(input("enter number of vertices: "))
 
@@ -25,12 +24,13 @@ for i in range(verts):
     degrees[i] = degrees[i] + AdjMat[i][j]
 
 
+#If a vertex has a degree greater than 2, the the graph is not only composed of paths and cycles
 for k in range(verts):
   if degrees[k]>2:
     print("Sorry, this graph is not composed of only paths and cycles as components")
     sys.exit()
 
-#Check for the case of a single node (A singloe node doesn't even have a path to itself, else if will be conseided a path)
+#Check for the case of a single node (A single node doesn't even have a path to itself, else if will be considered a path)
 for i in range(verts):
  if degrees[k]==0:
     print("Sorry, this graph is not composed of only paths and cycles as components, it contains a single node")
@@ -39,14 +39,15 @@ for i in range(verts):
 #We check the number of cycles and paths 
 visited_nodes =[0]*verts
 next_vert =0
-
 print('This graph has only paths and cycles as components.')
+
 flag=1
 SizesArray = []
 counter= 0
 while visited_nodes != [1]*verts:
+  
+  #First looks for unvisited nodes with degree 1 as they constitute the beginning of a path
   for i in range(verts):
-      #First looks for unvisited nodes with defree 1 as they constitute the beginning of a path
     if visited_nodes[i] == 0 and degrees[i]==1:
       next_vert = i
       visited_nodes[i] =1
@@ -80,8 +81,9 @@ while visited_nodes != [1]*verts:
       print ("The graph contains " + str(counter+1) + " Path(s) of size "+ str( SizesArray[i]))
  
   SizesArray = []
+
+  #Once all the paths have been traversed, it will look for beginning of cycles that have not been traversed 
   for i in range(verts):
-    #Once all the paths have been traversed, it will look for beginning of cycles that have not been traversed 
     if visited_nodes[i] == 0 and degrees[i]==2:
       next_vert = i
       visited_nodes[i] =1
